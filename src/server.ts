36f4bd482cf2bddcb04fe2cefe77cd7781e0f7e8
Dishant Tanwar <dishanttanwar@gmail.com>
2022-02-27 17:52:01 +0530

    ADD: cors support

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express, { Request, Response } from "express";
 import orderRoutes from "./handlers/orderRoutes";
 import productRoutes from "./handlers/productRoutes";
@@ -6,7 +7,12 @@ import userRoutes from "./handlers/userRoutes";
 
 const app: express.Application = express();
 const port = 3000;
+const corsOptions = {
+  origin: "http://example.com",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.get("/", function (req: Request, res: Response) {
